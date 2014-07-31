61671fd91a2c7efa4e6e71e3a0551ead03ffa26f
John DeSilva <desilvjo@umich.edu>
2014-07-30 22:48:19 -0400

    Add discogs helper

---
+++ discogs_helper.rb
@@ -0,0 +1,26 @@
+require 'discogs'
+
+module Wallbum
+  class DiscogsHelper
+    def initialize
+      @discogs_wrapper = Discogs::Wrapper.new('wallbum')
+    end
+
+    def get_discogs_artist_id(name)
+      @discogs_wrapper.search(name, type: :artist).results.first.id
+    end
+
+    def get_discogs_releases(artist_id)
+      @discogs_wrapper.get_artist_releases(artist_id).releases
+    end
+
+    def get_image_link(release)
+      begin
+        id = release.thumb.scan(%r!http://api\.discogs\.com/image/R-150-(.*)\.jpeg!).first.first
+        "http://s.pixogs.com/image/R-#{id}.jpeg"
+      rescue
+        nil
+      end
+    end
+  end
+end
