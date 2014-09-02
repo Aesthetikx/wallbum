aa1a7e7554ccc9cab38aaab767f25232e4b2dd60
John DeSilva <desilvjo@umich.edu>
2014-09-02 19:00:07 -0400

    Use slop for option parsing

--- wallbum.rb
+++ wallbum.rb
@@ -2,8 +2,23 @@ require_relative './discogs_helper.rb'
 require_relative './image_packer.rb'
 
 require 'fileutils'
+require 'slop'
+
+opts = Slop.parse! do
+  banner 'Usage: wallbum [-w 1920] [-h 1080] artist name'
+
+  on 'h', 'height=', 'Wallpaper Height'
+  on 'w', 'width=', 'Wallpaper Width'
+end
+
+if ARGV.empty?
+  puts opts.help
+  exit
+end
 
 artist_name = ARGV.join(" ")
+width = opts[:width].to_i || 1920
+height = opts[:height].to_i || 1080
 
 helper = Wallbum::DiscogsHelper.new
 
@@ -26,6 +41,6 @@ end
 
 fnames = Dir["#{dir}/*"].collect
 
-Wallbum::ImagePacking::Packer.new().pack_images(2560, 1600, fnames) do |wallp|
+Wallbum::ImagePacking::Packer.new().pack_images(width, height, fnames) do |wallp|
   wallp.write("#{artist_name.gsub(' ', '_')}.jpeg")
 end
